1f6a80328d8399a7777eb087f17e78554f5801b2
SrMotion666 <116975268+SrMotion666@users.noreply.github.com>
2025-07-14 18:51:06 +0800

    Update Nathubmain.lua

--- Nathubmain.lua
+++ Nathubmain.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/zBF3uJdXcU/0", true))()
-loadstring(game:HttpGet("https://get.nathub.xyz/loader"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))();
